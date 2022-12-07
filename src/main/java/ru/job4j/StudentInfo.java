fa3e162dfddda4677e0c1ea9fe9d151f8d0915a9
pasha-kzn <pavelmatweeff@yandex.ru>
2022-12-07 22:16:57 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Matveev");
+        System.out.println("20.05.1980");
+    }
+}
