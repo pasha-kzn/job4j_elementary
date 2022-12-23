020b0b8fb19f251ffc0aa5d5e49e78ee08942b38
pasha-kzn <pavelmatweeff@yandex.ru>
2022-12-24 00:53:37 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(420);
+        System.out.println("420 rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(420);
+        System.out.println("420 rubles are " + dollar + " dollar.");
+    }
+}
