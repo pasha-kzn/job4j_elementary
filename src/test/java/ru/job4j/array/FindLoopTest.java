45ef3576abda8cb4f5f7859851f5303a6607b6fa
pasha-kzn <pavelmatweeff@yandex.ru>
2023-01-05 13:14:05 +0300

    6.1. Классический поиск перебором. [#33489 #377710]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHasNot9ThenMinus1() {
+        /* Здесь нужно дописать тест, когда в массиве не найдено число 10.
+        Можно искать любое другое число, корректно изменив при этом название теста */
+        int[] data = new int[] {5, 10, 3};
+        int el = 9;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
