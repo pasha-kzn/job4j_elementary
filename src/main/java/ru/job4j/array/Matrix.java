0cbdb4f072e1bdfbb0bdf1549a3d04868094c727
pasha-kzn <pavelmatweeff@yandex.ru>
2023-01-07 15:41:06 +0300

    нарушения по стилю

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -6,7 +6,7 @@ public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
         for (int i = 0; i < size; i++) {
-            for ( int j = 0; j < size; j++) {
+            for (int j = 0; j < size; j++) {
                 array[i][j] = (i + 1) * (j + 1);
             }
         }
