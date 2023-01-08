88fa9134c030129b2d4c28417a3db80a2d4b35e9
pasha-kzn <pavelmatweeff@yandex.ru>
2023-01-08 13:17:56 +0300

    ХЗ чо такое

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -2,8 +2,7 @@ package ru.job4j.converter;
 
 import org.junit.Test;
 import org.junit.Assert;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import ru.job4j.condition.converter.Converter;
 
 public class ConverterTest {
 
