d270e2984ef972dd35f481c399a87a41e01caac8
pasha-kzn <pavelmatweeff@yandex.ru>
2023-01-18 22:01:22 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка активна: " + active);
+        System.out.println("Статус-код: " + status);
+        System.out.println("Сообщение: " + message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error err1 = new Error();
+        Error err2 = new Error(true, 404, "Not found");
+        Error err3 = new Error(false, 200, "OK");
+        Error err4 = new Error(true, 500, "Internal Server Error");
+        err1.printInfo();
+        err2.printInfo();
+        err3.printInfo();
+        err4.printInfo();
+    }
+}
