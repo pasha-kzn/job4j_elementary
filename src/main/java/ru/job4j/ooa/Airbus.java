829bc9e193c2d0888e13f9f7a6ac9d46878f6ae2
pasha-kzn <pavelmatweeff@yandex.ru>
2023-03-15 12:36:15 +0300

    final [#334073 #400897]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println("Количество двигателей равно: " + (this.getName().equals("A380") ? 4 : COUNT_ENGINE));
+        System.out.println("Количество двигателей равно: " + ("A380".equals(this.getName()) ? 4 : COUNT_ENGINE));
     }
 
     @Override
